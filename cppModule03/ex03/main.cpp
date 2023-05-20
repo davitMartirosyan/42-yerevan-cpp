b7ba241661a3411a12989d02e1cdf0de68feb694
Davit Martirosyan <dmartiro@c2r15s2.42yerevan.am>
2023-05-20 06:47:22 +0400

    CPP-03 Ex-03

--- cppModule03/ex03/main.cpp
+++ cppModule03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 07:33:04 by dmartiro          #+#    #+#             */
-/*   Updated: 2023/05/20 04:09:26 by dmartiro         ###   ########.fr       */
+/*   Updated: 2023/05/20 06:47:06 by dmartiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,5 @@ int main( void )
     diamond.whoAmI();
     diamond.attack("ol");
 }
+
+// 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
